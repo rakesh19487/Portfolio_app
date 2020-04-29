896c1ce7285bed52ef754f7d8a51f8213d3caa6a
Rakesh <rakesh@ptotem.com>
2020-04-29 18:32:31 +0530

    Added crud for portfolios and friendly id

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,44 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio, only: [:show, :update, :edit, :destroy]
+
+    def index
+        @portfolios = Portfolio.all
+    end    
+
+    def new
+        @portfolio = Portfolio.new
+    end    
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+
+        if @portfolio.save
+            redirect_to portfolios_path, notice: "Portfolio successfully created"
+        else
+            render :new, notice: "Something went wrong"    
+        end     
+    end     
+
+    def edit
+    end
+    
+    def update
+        @portfolio = Portfolio.update(portfolio_params)
+    end   
+    
+    def destroy
+        @portfolio.destroy
+        redirect_to portfolios_path, notice: "Portfolio Successfully deleted"
+    end 
+    
+    
+    private
+
+    def set_portfolio
+        @portfolio = Portfolio.friendly.find(params[:id])
+    end   
+    
+    def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image)
+    end   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+9.times do |p|
+    Portfolio.create!(
+        title: "This is title for portfolio #{p}",
+        subtitle: "This is subtitle for portfolio #{p}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        thumb_image: "https://via.placeholder.com/300",
+        main_image: "https://via.placeholder.com/640x400"
+    )
+end    
+
+
+10.times do |b|
+    Blog.create!(
+        title: "This is title for blog#{b}",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    )
+end    
+
+5.times do |s|
+    Skill.create!(
+        title: "Rails-#{s}",
+        percentage: 75
+    )
+end    
