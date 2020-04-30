f19dcf9b6fb292ffef7154ba475fe1ffdd78f941
Rakesh <rakesh@ptotem.com>
2020-04-30 17:49:50 +0530

    default values added after initialize

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,18 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :subtitle, :thumb_image, :main_image
 
+
+    after_initialize :set_defaults
+
     def self.order_by_created_at
         order(created_at: :desc)
     end    
+
+    def set_defaults
+        self.thumb_image ||= "https://via.placeholder.com/300"
+        self.main_image ||= "https://via.placeholder.com/640x400"
+    end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ end
         title: "This is title for portfolio #{p}",
         subtitle: "This is subtitle for portfolio #{p}",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        thumb_image: "https://via.placeholder.com/300",
-        main_image: "https://via.placeholder.com/640x400"
+        # thumb_image: "https://via.placeholder.com/300",
+        # main_image: "https://via.placeholder.com/640x400"
     )
 end    
 
