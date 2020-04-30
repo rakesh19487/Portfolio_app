6af7b6b10b65acea76d041ae961ded81a139132a
Rakesh <rakesh@ptotem.com>
2020-04-30 18:07:15 +0530

    Added model concern for default values

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,18 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :subtitle, :thumb_image, :main_image
 
-
-    after_initialize :set_defaults
+    include ImagePlaceholder
+    
+    after_initialize :set_image
 
     def self.order_by_created_at
         order(created_at: :desc)
     end    
 
-    def set_defaults
-        self.thumb_image ||= "https://via.placeholder.com/300"
-        self.main_image ||= "https://via.placeholder.com/640x400"
+
+    def set_image
+        self.thumb_image ||= ImagePlaceholder.image_generator(300, 300)
+        self.main_image ||= ImagePlaceholder.image_generator(600, 400)
     end
 
 
