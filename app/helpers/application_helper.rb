2497e2ddb5eb3bdd2ef45bb627fc6c8cf642825f
Rakesh <rakesh@ptotem.com>
2020-05-02 23:53:36 +0530

    added styling and layout for blog crud, portfolio show page and sortable site_admin authorization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-    def check_login_nav style
+    def check_login_nav(style = '', brtag)
          if current_user.is_a?(GuestUser)
-            (link_to 'Login', new_user_session_path, class: style) + "<br>".html_safe +
+            (link_to 'Login', new_user_session_path, class: style) + brtag.html_safe +
             (link_to 'Register', new_user_registration_path, class: style)
          else
             link_to "Logout", destroy_user_session_path, method: :delete, class: style
