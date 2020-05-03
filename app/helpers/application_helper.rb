cc2e310663b401311d065d2192b62af547a19256
Rakesh <rakesh@ptotem.com>
2020-05-03 21:31:49 +0530

    moved nav to common application helper and active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 
     def check_login_nav(style = '', brtag)
          if current_user.is_a?(GuestUser)
-            (link_to 'Login', new_user_session_path, class: style) + brtag.html_safe +
-            (link_to 'Register', new_user_registration_path, class: style)
+            (link_to 'Login', new_user_session_path, class: style + " " + set_active_class(new_user_session_path)) + brtag.html_safe +
+            (link_to 'Register', new_user_registration_path, class: style + " " + set_active_class(new_user_registration_path))
          else
             link_to "Logout", destroy_user_session_path, method: :delete, class: style
          end 
@@ -14,4 +14,48 @@ module ApplicationHelper
             content_tag :p, "Thanks for visiting from #{session[:source]}"
         end
     end    
+
+    def nav_links
+      [
+         {
+            nav_name: 'Home',
+            path: root_path
+         },
+         {
+            nav_name: 'About',
+            path: about_path
+         },
+         {
+            nav_name: 'Contact',
+            path: contact_path
+         },
+         {
+            nav_name: 'Blogs',
+            path: blogs_path
+         },
+         {
+            nav_name: 'Portfolios',
+            path: portfolios_path
+         }
+      ]
+    end  
+
+
+    def set_active_class(path_name)
+      active_class = ''
+      if current_page?(path_name)
+         active_class = 'active'
+      else
+         active_class = ''
+      end
+      active_class
+    end  
+
+    def set_nav_links(tag_attr, tag_style)
+      navs = ""
+      nav_links.each do |nav|
+          navs <<  "<#{tag_attr}>"+'<a class='+ "'" +"#{tag_style} #{set_active_class(nav[:path])}"+ "' href=#{nav[:path]}>#{nav[:nav_name]}</a>" + "</#{tag_attr}>" 
+      end   
+      navs.html_safe
+    end  
 end
